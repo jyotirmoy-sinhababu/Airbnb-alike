761191f9049051354a95fe7563e063bd9f9132e4
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-02-24 02:14:35 +0530

    logo comp created

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Logo from '../logo/Logo';
+
+const Nav = () => {
+  return (
+    <div>
+      <div>
+        <Logo />
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../../components/nav/Nav';
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+};
+
+export default Home;
